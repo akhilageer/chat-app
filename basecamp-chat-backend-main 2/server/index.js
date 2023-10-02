846a107949ded403e78bb2ead7a45355b6d57bd5
Akhil Ageer <Akhilguptha99@gmail.com>
2023-10-01 23:44:48 -0400

    Update index.js

--- basecamp-chat-backend-main 2/server/index.js
+++ basecamp-chat-backend-main 2/server/index.js
@@ -15,6 +15,24 @@ import ChatRoom from "./models/ChatRoom.js";
 const app = express();
 const path  = require('path');
 
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname  , "../client/build");
+
+app.use(express.static(buildPath))
+
+app.get("/*", function(req, res){
+
+    res.sendFile(
+        path.join(__dirname, "../client/build/index.html"),
+        function (err) {
+          if (err) {
+            res.status(500).send(err);
+          }
+        }
+      );
+
+})
+
 dotenv.config();
 
 app.use(cors());
